948fcdf0bcbb2739d3d51af135d35588a1043028
Leo Wong <pakyinwww@gmail.com>
2026-03-04 23:10:38 +0000

    feat: update about page

--- apps/popup/src/pages/About.tsx
+++ apps/popup/src/pages/About.tsx
@@ -1,8 +1,27 @@
-import { Title } from '@mantine/core';
-import { useTranslation } from 'react-i18next';
+import { Title, Text, Stack, ActionIcon } from "@mantine/core";
+import { IconLink } from "@tabler/icons-react";
+import { useTranslation } from "react-i18next";
 
 export function About() {
   const { t } = useTranslation();
-  return <Title order={2}>{t('app.nav.about')}</Title>;
+  return (
+    <Stack gap="md">
+      <Title order={2}>{t("app.nav.about")}</Title>
+      <Text size="sm" c="dimmed">
+        {t("app.nav.copyright", { year: new Date().getFullYear() })} {t("app.nav.owner")} <ActionIcon
+          variant="transparent"
+          size="xs"
+          component="a"
+          color="red"
+          href="https://www.hanamaru.hk"
+          target="_blank"
+        >
+          <IconLink />
+        </ActionIcon>
+      </Text>
+      <Text size="xs" c="dimmed">
+        {t("app.nav.disclaimer")}
+      </Text>
+    </Stack>
+  );
 }
-
